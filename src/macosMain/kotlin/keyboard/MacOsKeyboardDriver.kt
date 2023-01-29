7cdd3b945e10fc83bbbced31e6cf930da17ee455
Micha Lehmann <michalehmann0112@gmail.com>
2023-01-29 14:48:01 +0100

    Add not-yet-implemented MacOS keyboard driver

---
+++ src/macosMain/kotlin/keyboard/MacOsKeyboardDriver.kt
@@ -0,0 +1,9 @@
+package keyboard
+
+import NotYetImplementedForThisPlatform
+
+object MacOsKeyboardDriver : KeyboardDriver {
+    override fun press(key: Key) = NotYetImplementedForThisPlatform()
+
+    override fun release(key: Key) = NotYetImplementedForThisPlatform()
+}
